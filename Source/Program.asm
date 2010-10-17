f248959f53f757838790f64ff16d5aa7e2a00aca
Ben Ryves <benryves@benryves.com>
2010-10-17 13:14:14 +0000

    Added a line plotter so you can see the walls.

--- Source/Program.asm
+++ Source/Program.asm
@@ -6,55 +6,16 @@ Loop:
 
 	.bcall _GrBufClr
 
-	ld hl,Vertices
-	ld de,saveSScreen
-	ld bc,Vertices.Count
-	call Nostromo.Vertices.Transform
-	
-	ld hl,saveSScreen
-	ld b,Vertices.Count
--:	inc hl
-	ld a,(hl)
-	inc hl
-	
-	add a,48
-	ld d,a
-	
-	inc hl
-	ld a,(hl)
-	inc hl
-	
-	neg
-	add a,32
-	ld e,a
-	
-	push bc
-	push hl
-	
-	cp 64
-	jr nc,+
-	
-	ld a,d
-	cp 96
-	jr nc,+
-	
-	call ionGetPixel
-	or (hl)
-	ld (hl),a
-	
-+:
-	
-	pop hl
-	pop bc
-	
-	djnz -
+	call TransformVertices
+	;call PlotVertices
+	call PlotWalls
 	
 	; Walk the tree.
 	xor a
 	ld (penCol),a
 	ld (penRow),a
 	ld ix,Tree
-	call WalkTree
+	;call WalkTree
 	
 	call ionFastCopy
 	
@@ -134,6 +95,136 @@ Loop:
 +:
 	jp Loop
 
+
+TransformVertices:
+	ld hl,Vertices
+	ld de,saveSScreen
+	ld bc,Vertices.Count
+	jp Nostromo.Vertices.Transform	
+
+PlotVertices:
+	ld hl,saveSScreen
+	ld b,Vertices.Count
+	
+-:	inc hl
+	ld a,(hl)
+	inc hl
+	
+	add a,48
+	ld d,a
+	
+	inc hl
+	ld a,(hl)
+	inc hl
+	
+	neg
+	add a,32
+	ld e,a
+	
+	push bc
+	push hl
+	
+	cp 64
+	jr nc,+
+	
+	ld a,d
+	cp 96
+	jr nc,+
+	
+	call ionGetPixel
+	or (hl)
+	ld (hl),a
+	
++:
+	
+	pop hl
+	pop bc
+	
+	djnz -
+	ret
+
+PlotWalls:
+	ld hl,Walls
+	ld b,Walls.Count
+
+PlotWalls.Loop:
+	push hl
+	
+	ld l,(hl)
+	ld h,0
+	add hl,hl
+	add hl,hl
+	ld de,saveSScreen
+	add hl,de
+	
+	ld e,(hl)
+	inc hl
+	ld d,(hl)
+	inc hl
+	ld (Wall.Start.X),de
+	
+	ld e,(hl)
+	inc hl
+	ld d,(hl)
+	ld (Wall.Start.Y),de
+	
+	pop hl
+	inc hl
+	push hl
+	
+	ld l,(hl)
+	ld h,0
+	add hl,hl
+	add hl,hl
+	ld de,saveSScreen
+	add hl,de
+	
+	ld e,(hl)
+	inc hl
+	ld d,(hl)
+	inc hl
+	ld (Wall.End.X),de
+	
+	ld e,(hl)
+	inc hl
+	ld d,(hl)
+	ld (Wall.End.Y),de
+	
+	pop hl
+	inc hl
+
+	push hl
+	push bc
+	
+	ld a,(Wall.Start.X+1)
+	add a,48
+	ld d,a
+	ld a,(Wall.Start.Y+1)
+	neg
+	add a,32
+	ld e,a
+	
+	ld a,(Wall.End.X+1)
+	add a,48
+	ld h,a
+	ld a,(Wall.End.Y+1)
+	neg
+	add a,32
+	ld l,a
+
+	call lineClipAndDrawLong
+	
+	pop bc
+	pop hl
+	djnz PlotWalls.Loop
+	ret
+	
+	
+Wall.Start.X: .dw 0
+Wall.Start.Y: .dw 0
+Wall.End.X: .dw 0
+Wall.End.Y: .dw 0	
+
 WalkTree:
 	ld a,(ix+0)
 	or a
@@ -209,3 +300,5 @@ Forwards.Y: .dw 0
 
 #include "Level.inc"
 #include "Nostromo/Nostromo.asm"
+#include "LINECLIP.INC"
+#include "LINEDRAW.INC"
