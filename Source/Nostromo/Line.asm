f511ecd8830f2b5252db55a4ae86dcd52b060f44
Ben Ryves <benryves@benryves.com>
2010-10-24 20:41:11 +0000

    Further optimised the line drawing routine - we're now up to 7.24 fps.

--- Source/Nostromo/Line.asm
+++ Source/Nostromo/Line.asm
@@ -64,16 +64,42 @@ SetSteepness:
 	
 	; dy = |y1 - y0|
 	; ystep = sgn(y1 - y0)
-	ld l,$2C ; inc l
 	ld a,e
 	sub c
-	jp p,+
-	neg
-	inc l ; dec l
-+:	ld (Delta.Y),a
+	jp m,Delta.Y.Negative
 
-	ld a,l
+Delta.Y.Positive:
+	ld (Delta.Y),a
+	
+	ld a,$2C ; inc l
 	ld (YStep),a
+	
+	ld hl,+12
+	ld (AdvanceY.Shallow.Stride),hl
+
+	ld a,$0F ; rrca
+	ld (AdvanceY.Steep.Shift),a
+	ld a,$23 ; inc hl
+	ld (AdvanceY.Steep.Increment),a
+	
+	jr Delta.Y.Set
+
+Delta.Y.Negative:
+	neg
+	ld (Delta.Y),a
+	
+	ld a,$2D ; dec l
+	ld (YStep),a
+	
+	ld hl,-12
+	ld (AdvanceY.Shallow.Stride),hl
+
+	ld a,$07 ; rlca
+	ld (AdvanceY.Steep.Shift),a
+	ld a,$2B ; dec hl
+	ld (AdvanceY.Steep.Increment),a
+
+Delta.Y.Set:
 
 	; a = (x1 - x0) + 1 (number of steps).
 	ld a,d
@@ -88,26 +114,70 @@ SetSteepness:
 	ld a,(Error)
 	ld c,a
 	
+	push hl
+	push bc
+	
+	bit LineFlag.Steep,(iy+LineFlags)
+	jr nz,+
+	ld a,h
+	ld e,l
+	jr ++
++:	ld a,l
+	ld e,h
+++:	
+	
+	call ionGetPixel
+	ld (PixelBufferOffset),hl
+	ld (PixelMask),a
+	
+	pop bc
+	pop hl
+	
 -:	push hl
 	push bc
 	
 	bit LineFlag.Steep,(iy+LineFlags)
 	jr nz,+
-	ld a,h \ ld h,l \ ld l,a	
+	ld a,h \ ld h,l \ ld l,a
 +:
 
 ClipPixel = $+1
-	call 0
+	call NoClip
 	jr c,Line.PixelClipped
 
-	; Plot (L,H)
-	ld a,l
-	ld e,h
-	call ionGetPixel
-	or (hl)
+PixelBufferOffset = $+1
+	ld hl,0
+	
+	ld a,(hl)
+PixelMask = $+1
+	or 0	
+	
 	ld (hl),a
-
+	
 Line.PixelClipped:
+
+	; Advance X.
+	bit LineFlag.Steep,(iy+LineFlags)
+	jr nz,AdvanceX.Steep
+
+AdvanceX.Shallow:
+	ld a,(PixelMask)
+	rrca
+	ld (PixelMask),a
+	jr nc,+
+	ld hl,(PixelBufferOffset)
+	inc hl
+	ld (PixelBufferOffset),hl
++:
+	jr AdvanceX.Done
+
+AdvanceX.Steep:
+	ld hl,(PixelBufferOffset)
+	ld bc,12
+	add hl,bc
+	ld (PixelBufferOffset),hl
+
+AdvanceX.Done:
 	
 	pop bc
 	pop hl
@@ -117,15 +187,62 @@ Delta.Y = $+1
 	ld de,0
 	ld a,c
 	sub e
-	jp p,+
+	jp p,NoAdvanceY
+	
+	; Advance Y.
+	bit LineFlag.Steep,(iy+LineFlags)
+	jr nz,AdvanceY.Steep
+
+AdvanceY.Shallow:
+	push hl
+	push bc
+	ld hl,(PixelBufferOffset)
+AdvanceY.Shallow.Stride = $+1
+	ld bc,12
+	add hl,bc
+	ld (PixelBufferOffset),hl
+	pop bc
+	pop hl
+	jr AdvanceY.Done
+
+AdvanceY.Steep:
+	push af
+	ld a,(PixelMask)
+AdvanceY.Steep.Shift:
+	rrca
+	ld (PixelMask),a
+	jr nc,+
+	push hl
+	ld hl,(PixelBufferOffset)
+AdvanceY.Steep.Increment:
+	inc hl
+	ld (PixelBufferOffset),hl
+	pop hl
++:
+	pop af
+
+AdvanceY.Done:	
+	
 YStep:
 	inc l
-	add a,d
-+:	ld c,a
+	add a,d	
+	
+NoAdvanceY:
+	ld c,a
 
 	inc h ; ++x
-	djnz -
+	djnz +
 
+	ret
++:	jp -
+
+; ==========================================================================
+; Line.NoClip
+; --------------------------------------------------------------------------
+; Dummy clipping routine that never clips the line.
+; ==========================================================================
+NoClip
+	or a
 	ret
 
 .endmodule
