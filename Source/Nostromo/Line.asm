4fce2ce42981987f354ed77d8a2ee102fbc585a5
Ben Ryves <benryves@benryves.com>
2010-10-30 14:21:54 +0000

    Split the wall-drawing code into two (currently identical) code paths, one for steep and one for shallow.

--- Source/Nostromo/Line.asm
+++ Source/Nostromo/Line.asm
@@ -5,6 +5,9 @@ LineFlag.Steep = 7
 
 Error: .db 0
 
+ClipPixel: .dw 0
+Delta.X: .db 0
+
 ; ==========================================================================
 ; Line.Draw
 ; --------------------------------------------------------------------------
@@ -41,13 +44,17 @@ Steep:
 	set LineFlag.Steep,(iy+LineFlags)
 	ld a,b \ ld b,c \ ld c,a
 	ld a,d \ ld d,e \ ld e,a
-	jr SetSteepness
+	jp Draw.Steep
 	
 NotSteep:
 	res LineFlag.Steep,(iy+LineFlags)
+	jp Draw.Shallow
+	
 
-SetSteepness:
-
+; --------------------------------------------------------------------------
+; Draw a shallow line (|dX| > |dY|)
+; --------------------------------------------------------------------------
+Draw.Shallow:
 	; dx = x1 - x0
 	ld a,d
 	sub b
@@ -59,7 +66,7 @@ SetSteepness:
 	ld a,c \ ld c,e \ ld e,a
 	ld a,l
 +:
-	ld (Delta.X),a
+	ld (Shallow.Delta.X),a
 	
 	; error = dx / 2
 	srl a
@@ -69,40 +76,43 @@ SetSteepness:
 	; ystep = sgn(y1 - y0)
 	ld a,e
 	sub c
-	jp m,Delta.Y.Negative
+	jp m,Shallow.Delta.Y.Negative
 
-Delta.Y.Positive:
-	ld (Delta.Y),a
+Shallow.Delta.Y.Positive:
+	ld (Shallow.Delta.Y),a
 	
 	ld a,$2C ; inc l
-	ld (YStep),a
+	ld (Shallow.YStep),a
 	
 	ld hl,+12
-	ld (AdvanceY.Shallow.Stride),hl
+	ld (Shallow.AdvanceY.Shallow.Stride),hl
 
 	ld a,$0F ; rrca
-	ld (AdvanceY.Steep.Shift),a
+	ld (Shallow.AdvanceY.Steep.Shift),a
 	ld a,$23 ; inc hl
-	ld (AdvanceY.Steep.Increment),a
+	ld (Shallow.AdvanceY.Steep.Increment),a
 	
-	jr Delta.Y.Set
+	jr Shallow.Delta.Y.Set
 
-Delta.Y.Negative:
+Shallow.Delta.Y.Negative:
 	neg
-	ld (Delta.Y),a
+	ld (Shallow.Delta.Y),a
 	
 	ld a,$2D ; dec l
-	ld (YStep),a
+	ld (Shallow.YStep),a
 	
 	ld hl,-12
-	ld (AdvanceY.Shallow.Stride),hl
+	ld (Shallow.AdvanceY.Shallow.Stride),hl
 
 	ld a,$07 ; rlca
-	ld (AdvanceY.Steep.Shift),a
+	ld (Shallow.AdvanceY.Steep.Shift),a
 	ld a,$2B ; dec hl
-	ld (AdvanceY.Steep.Increment),a
+	ld (Shallow.AdvanceY.Steep.Increment),a
 
-Delta.Y.Set:
+Shallow.Delta.Y.Set:
+
+	ld hl,(ClipPixel)
+	ld (Shallow.ClipPixel),hl
 
 	; a = (x1 - x0) + 1 (number of steps).
 	ld a,d
@@ -131,8 +141,8 @@ Delta.Y.Set:
 	
 	dec e
 	call ionGetPixel
-	ld (PixelBufferOffset),hl
-	ld (PixelMask),a
+	ld (Shallow.PixelBufferOffset),hl
+	ld (Shallow.PixelMask),a
 	
 	pop bc
 	pop hl
@@ -145,100 +155,295 @@ Delta.Y.Set:
 	ld a,h \ ld h,l \ ld l,a
 +:
 
-ClipPixel = $+1
+Shallow.ClipPixel = $+1
 	call NoClip
-	jr c,Line.PixelClipped
+	jr c,Shallow.Line.PixelClipped
 
-PixelBufferOffset = $+1
+Shallow.PixelBufferOffset = $+1
 	ld hl,0
 	
 	ld a,(hl)
-PixelMask = $+1
+Shallow.PixelMask = $+1
 	or 0	
 	
 	ld (hl),a
 	
-Line.PixelClipped:
+Shallow.Line.PixelClipped:
 
 	; Advance X.
 	bit LineFlag.Steep,(iy+LineFlags)
-	jr nz,AdvanceX.Steep
+	jr nz,Shallow.AdvanceX.Steep
 
-AdvanceX.Shallow:
-	ld a,(PixelMask)
+Shallow.AdvanceX.Shallow:
+	ld a,(Shallow.PixelMask)
 	rrca
-	ld (PixelMask),a
+	ld (Shallow.PixelMask),a
 	jr nc,+
-	ld hl,(PixelBufferOffset)
+	ld hl,(Shallow.PixelBufferOffset)
 	inc hl
-	ld (PixelBufferOffset),hl
+	ld (Shallow.PixelBufferOffset),hl
 +:
-	jr AdvanceX.Done
+	jr Shallow.AdvanceX.Done
 
-AdvanceX.Steep:
-	ld hl,(PixelBufferOffset)
+Shallow.AdvanceX.Steep:
+	ld hl,(Shallow.PixelBufferOffset)
 	ld bc,12
 	add hl,bc
-	ld (PixelBufferOffset),hl
+	ld (Shallow.PixelBufferOffset),hl
 
-AdvanceX.Done:
+Shallow.AdvanceX.Done:
 	
 	pop bc
 	pop hl
 
-Delta.X = $+2
-Delta.Y = $+1
+Shallow.Delta.X = $+2
+Shallow.Delta.Y = $+1
 	ld de,0
 	ld a,c
 	sub e
-	jp p,NoAdvanceY
+	jp p,Shallow.NoAdvanceY
 	
 	; Advance Y.
 	bit LineFlag.Steep,(iy+LineFlags)
-	jr nz,AdvanceY.Steep
+	jr nz,Shallow.AdvanceY.Steep
 
-AdvanceY.Shallow:
+Shallow.AdvanceY.Shallow:
 	push hl
 	push bc
-	ld hl,(PixelBufferOffset)
-AdvanceY.Shallow.Stride = $+1
+	ld hl,(Shallow.PixelBufferOffset)
+Shallow.AdvanceY.Shallow.Stride = $+1
 	ld bc,12
 	add hl,bc
-	ld (PixelBufferOffset),hl
+	ld (Shallow.PixelBufferOffset),hl
 	pop bc
 	pop hl
-	jr AdvanceY.Done
+	jr Shallow.AdvanceY.Done
 
-AdvanceY.Steep:
+Shallow.AdvanceY.Steep:
 	push af
-	ld a,(PixelMask)
-AdvanceY.Steep.Shift:
+	ld a,(Shallow.PixelMask)
+Shallow.AdvanceY.Steep.Shift:
 	rrca
-	ld (PixelMask),a
+	ld (Shallow.PixelMask),a
 	jr nc,+
 	push hl
-	ld hl,(PixelBufferOffset)
-AdvanceY.Steep.Increment:
+	ld hl,(Shallow.PixelBufferOffset)
+Shallow.AdvanceY.Steep.Increment:
 	inc hl
-	ld (PixelBufferOffset),hl
+	ld (Shallow.PixelBufferOffset),hl
 	pop hl
 +:
 	pop af
 
-AdvanceY.Done:	
+Shallow.AdvanceY.Done:
 	
-YStep:
+Shallow.YStep:
 	inc l
 	add a,d	
 	
-NoAdvanceY:
+Shallow.NoAdvanceY:
 	ld c,a
 
 	inc h ; ++x
-	djnz +
-
+	djnz -
 	ret
-+:	jp -
+
+; --------------------------------------------------------------------------
+; Draw a steep line (|dX| > |dY|)
+; --------------------------------------------------------------------------
+Draw.Steep:
+	; dx = x1 - x0
+	ld a,d
+	sub b
+	jp p,+
+
+	neg
+	ld l,a
+	ld a,b \ ld b,d \ ld d,a
+	ld a,c \ ld c,e \ ld e,a
+	ld a,l
++:
+	ld (Steep.Delta.X),a
+	
+	; error = dx / 2
+	srl a
+	ld (Error),a
+	
+	; dy = |y1 - y0|
+	; ystep = sgn(y1 - y0)
+	ld a,e
+	sub c
+	jp m,Steep.Delta.Y.Negative
+
+Steep.Delta.Y.Positive:
+	ld (Steep.Delta.Y),a
+	
+	ld a,$2C ; inc l
+	ld (Steep.YStep),a
+	
+	ld hl,+12
+	ld (Steep.AdvanceY.Shallow.Stride),hl
+
+	ld a,$0F ; rrca
+	ld (Steep.AdvanceY.Steep.Shift),a
+	ld a,$23 ; inc hl
+	ld (Steep.AdvanceY.Steep.Increment),a
+	
+	jr Steep.Delta.Y.Set
+
+Steep.Delta.Y.Negative:
+	neg
+	ld (Steep.Delta.Y),a
+	
+	ld a,$2D ; dec l
+	ld (Steep.YStep),a
+	
+	ld hl,-12
+	ld (Steep.AdvanceY.Shallow.Stride),hl
+
+	ld a,$07 ; rlca
+	ld (Steep.AdvanceY.Steep.Shift),a
+	ld a,$2B ; dec hl
+	ld (Steep.AdvanceY.Steep.Increment),a
+
+Steep.Delta.Y.Set:
+
+	ld hl,(ClipPixel)
+	ld (Steep.ClipPixel),hl
+
+	; a = (x1 - x0) + 1 (number of steps).
+	ld a,d
+	sub b
+	inc a
+	
+	ld h,b ; Start X.
+	ld l,c ; Start Y.
+	
+	ld b,a ; b = number of steps.
+	
+	ld a,(Error)
+	ld c,a
+	
+	push hl
+	push bc
+	
+	bit LineFlag.Steep,(iy+LineFlags)
+	jr nz,+
+	ld a,h
+	ld e,l
+	jr ++
++:	ld a,l
+	ld e,h
+++:	
+	
+	dec e
+	call ionGetPixel
+	ld (Steep.PixelBufferOffset),hl
+	ld (Steep.PixelMask),a
+	
+	pop bc
+	pop hl
+	
+-:	push hl
+	push bc
+	
+	bit LineFlag.Steep,(iy+LineFlags)
+	jr nz,+
+	ld a,h \ ld h,l \ ld l,a
++:
+
+Steep.ClipPixel = $+1
+	call NoClip
+	jr c,Steep.Line.PixelClipped
+
+Steep.PixelBufferOffset = $+1
+	ld hl,0
+	
+	ld a,(hl)
+Steep.PixelMask = $+1
+	or 0	
+	
+	ld (hl),a
+
+Steep.Line.PixelClipped:
+
+	; Advance X.
+	bit LineFlag.Steep,(iy+LineFlags)
+	jr nz,Steep.AdvanceX.Steep
+
+Steep.AdvanceX.Shallow:
+	ld a,(Steep.PixelMask)
+	rrca
+	ld (Steep.PixelMask),a
+	jr nc,+
+	ld hl,(Steep.PixelBufferOffset)
+	inc hl
+	ld (Steep.PixelBufferOffset),hl
++:
+	jr Steep.AdvanceX.Done
+
+Steep.AdvanceX.Steep:
+	ld hl,(Steep.PixelBufferOffset)
+	ld bc,12
+	add hl,bc
+	ld (Steep.PixelBufferOffset),hl
+
+Steep.AdvanceX.Done:
+	
+	pop bc
+	pop hl
+
+Steep.Delta.X = $+2
+Steep.Delta.Y = $+1
+	ld de,0
+	ld a,c
+	sub e
+	jp p,Steep.NoAdvanceY
+	
+	; Advance Y.
+	bit LineFlag.Steep,(iy+LineFlags)
+	jr nz,Steep.AdvanceY.Steep
+
+Steep.AdvanceY.Shallow:
+	push hl
+	push bc
+	ld hl,(Steep.PixelBufferOffset)
+Steep.AdvanceY.Shallow.Stride = $+1
+	ld bc,12
+	add hl,bc
+	ld (Steep.PixelBufferOffset),hl
+	pop bc
+	pop hl
+	jr Steep.AdvanceY.Done
+
+Steep.AdvanceY.Steep:
+	push af
+	ld a,(Steep.PixelMask)
+Steep.AdvanceY.Steep.Shift:
+	rrca
+	ld (Steep.PixelMask),a
+	jr nc,+
+	push hl
+	ld hl,(Steep.PixelBufferOffset)
+Steep.AdvanceY.Steep.Increment:
+	inc hl
+	ld (Steep.PixelBufferOffset),hl
+	pop hl
++:
+	pop af
+
+Steep.AdvanceY.Done:
+	
+Steep.YStep:
+	inc l
+	add a,d	
+	
+Steep.NoAdvanceY:
+	ld c,a
+
+	inc h ; ++x
+	djnz -
+	ret
 
 ; ==========================================================================
 ; Line.NoClip
