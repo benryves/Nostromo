01130ce8f052df2d301cef7e17d6f65c7726ec28
Ben Ryves <benryves@benryves.com>
2010-11-07 14:19:21 +0000

    Added explicit checks for the LCD busy flag. This should allow the program to run properly at 15MHz.

--- Source/Nostromo/Screen.asm
+++ Source/Nostromo/Screen.asm
@@ -1,33 +1,37 @@
 .module Screen
 
+; ==========================================================================
+; Copy
+; --------------------------------------------------------------------------
+; Copies the screen buffer to the display.
+; --------------------------------------------------------------------------
+; Destroyed: AF, BC, DE, HL, IX.
+; ==========================================================================
 Copy:
 	di
-	ld	a,$80				; 7
-	out	($10),a				; 11
-	ld	hl,plotSScreen-12-(-(12*64)+1)		; 10
-	ld	a,$20				; 7
-	ld	c,a				; 4
-	inc	hl				; 6 waste
-	dec	hl				; 6 waste
+-:	in a,($10) \ rla \ jr c,-
+	ld a,$80
+	out ($10),a
+	ld hl,plotSScreen-12-(-(12*64)+1)
+	ld c,$20
 fastCopyAgain:
-	ld	b,64				; 7
-	inc	c				; 4
-	ld	de,-(12*64)+1			; 10
-	out	($10),a				; 11
-	add	hl,de				; 11
-	ld	de,10				; 10
+	ld b,64
+	ld de,-(12*64)+1
+-:	in a,($10) \ rla \ jr c,-
+	ld a,c
+	out ($10),a
+	add hl,de
+	ld de,12
 fastCopyLoop:
-	add	hl,de				; 11
-	inc	hl				; 6 waste
-	inc	hl				; 6 waste
-	inc	de				; 6
-	ld	a,(hl)				; 7
-	out	($11),a				; 11
-	dec	de				; 6
-	djnz	fastCopyLoop			; 13/8
-	ld	a,c				; 4
-	cp	$2B+1				; 7
-	jr	nz,fastCopyAgain		; 10/1
-	ret					; 10
+	add hl,de
+-:	in a,($10) \ rla \ jr c,-
+	ld a,(hl)
+	out ($11),a
+	djnz fastCopyLoop
+	inc c
+	ld a,c
+	cp $2B+1
+	jr nz,fastCopyAgain
+	ret
 
 .endmodule
